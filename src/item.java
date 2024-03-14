d47b1b15c591cec5e00b6e8623473054ebf9c7d2
Duyen-03 <157661508+Duyen-03@users.noreply.github.com>
2024-03-14 07:48:16 +0700

    Update and rename Main.java to item.java

--- src/item.java
+++ src/item.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        MP3 mp3;
+        MP3 mp3 = new MP3();
         mp3.showInfo();
 
-        Book book;
+        Book book = new Book();
         book.showInfo();
 
     }
